9de82a4ecddca2cf88cb36d883bbcc2697da4fff
Robert van de Vlasakker <robertvandevlasakker@outlook.com>
2021-09-17 15:16:41 +0200

    backup

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -10,6 +10,8 @@ from transformers import DistilBertTokenizer, DistilBertModel
     
 # Load the BERT tokenizer
 tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')  
+bert = DistilBertModel.from_pretrained('distilbert-base-uncased')
+warnings.filterwarnings("ignore")
 
 def loadBERT(location, modelname):
     
@@ -18,10 +20,8 @@ def loadBERT(location, modelname):
     """
     
     warnings.filterwarnings("ignore")
-
     device = 'cuda' if cuda.is_available() else 'cpu'
-    
-    bert = DistilBertModel.from_pretrained('distilbert-base-uncased')
+    #bert = DistilBertModel.from_pretrained('distilbert-base-uncased')
 
     
     class BERT(nn.Module):
@@ -86,6 +86,38 @@ def loadBERT(location, modelname):
     
     return model
 
+def load_simBERT():
+    
+    """
+    Load BERT for sentence similarity.
+    """
+    
+    device = 'cuda' if cuda.is_available() else 'cpu'
+    #bert = DistilBertModel.from_pretrained('distilbert-base-uncased')
+    
+    class BERT(nn.Module):
+        def __init__(self, bert):
+            super(BERT, self).__init__()
+
+            # Distil Bert model
+            self.bert = bert
+
+        #define the forward pass
+        def forward(self, **kwargs):
+
+            #pass the inputs to the model BERT  
+            cls_hs = self.bert(**kwargs)
+            hidden_state = cls_hs.last_hidden_state
+
+            return hidden_state
+        
+    model = BERT(bert)
+    # push the model to GPU
+    model = model.to(device)
+    # Eval mode
+    model.eval()
+    
+    return model
 
 def SpanPredictor(span, model, pred_values=False):
 
@@ -110,5 +142,38 @@ def SpanPredictor(span, model, pred_values=False):
         else:    
             return span_class
         
+        
+def similarity_matrix(sentence_list, model):
+    
+    """
+    Calculates a similarity cosine matrix based on a list of
+    sentences.
+    """
+    
+    # Initialize dictionary to store tokenized sentences
+    tokens = {'input_ids': [], 'attention_mask': []}
+
+    for sentence in sentence_list:
+        # encode each sentence and append to dictionary
+        new_tokens = tokenizer.encode_plus(sentence, max_length=512,
+                                           truncation=True, 
+                                           padding='max_length',
+                                           return_tensors='pt')
+        # Drop the batch dimension
+        tokens['input_ids'].append(new_tokens['input_ids'][0])
+        tokens['attention_mask'].append(new_tokens['attention_mask'][0])
+    
+    # Reformat list of tensors into single tensor
+    tokens['input_ids'] = torch.stack(tokens['input_ids'])
+    tokens['attention_mask'] = torch.stack(tokens['attention_mask'])
+    
+    # Get vectors
+    hiddenstates = sim_model(**tokens)
+    # Sum along first axis
+    summed_hs = torch.sum(hiddenstates, 1)
+    # Detach
+    summed_hs_np = summed_hs.detach().numpy()
+    # Get the matrix
+    return summed_hs_np
 
     
